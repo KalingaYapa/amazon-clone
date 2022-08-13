59361f1c6f73ca6521562fbfb9535c0cbf3a81b8
KalingaY <y.kalinga@directfn.com>
2022-08-14 03:00:35 +0530

    Navigation design

--- amazon-clone-app/src/Component/NavBar/Navigation.js
+++ amazon-clone-app/src/Component/NavBar/Navigation.js
@@ -11,8 +11,26 @@ class NavBar extends Component {
             <div className='navbar_component'> 
                 <div className='navbar_logo'></div>
                 <div className='navbar_locator'>
-                <div className='navbar_location_image'></div>
-                <div className='navbar_location'> Sri Lanka</div>
+                    <div className='navbar_location_image'></div>
+                    <div className='navbar_location'> Sri Lanka</div>
+                </div>
+                <div className='navbar_search'>
+                    <div className='navbar_dropdown'>
+                        <select className='navbar_select'> 
+                            <option>All</option>
+                            <option>Baby</option>
+                            <option>Beauty</option>
+                            <option>Text</option>
+                            <option>Deals</option>
+                        </select>
+                    </div>
+                    <div className='navbar_textbox'>
+                        <input className='navbar_input'></input>
+                    </div>
+                    <div className='navbar_search_icon_div'>
+                        <div className='navbar_search_icon'></div>
+                    </div>
+                        
                 </div>
                 
             </div> 
