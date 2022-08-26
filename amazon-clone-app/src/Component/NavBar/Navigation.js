38f992279c4727445dda1361b70ecf8f8f4e94f5
KalingaY <y.kalinga@directfn.com>
2022-08-27 00:52:38 +0530

    Top Panel Completed

--- amazon-clone-app/src/Component/NavBar/Navigation.js
+++ amazon-clone-app/src/Component/NavBar/Navigation.js
@@ -5,6 +5,7 @@ class NavBar extends Component {
     constructor(props) {
         super(props);
     }
+    
     state = {  }
     render() { 
         return ( 
@@ -29,6 +30,20 @@ class NavBar extends Component {
                     </div>
                     <div className='navbar_search_icon_div'>
                         <div className='navbar_search_icon'></div>
+                    </div>
+
+                    <div className='navbar_text navbar_sign_in'>
+                        <div style={{fontSize: "12px", fontWeight : "400"}} >Hello Sign in</div>
+                        <div style={{fontWeight : "700"}}> Account & List</div>
+                    </div>
+                    <div className='navbar_text navbar_return_orders'>
+                        <div style={{fontSize: "12px", fontWeight : "400"}}>Returns </div>
+                        <div style={{fontWeight : "700"}}>& Orders</div>
+                    </div>
+                    <div className='navbar_text navbar_cart'>
+                        <div className='navbar_cart_image'> </div>
+                        <div className='navbar_cart_item'>0</div>
+                        <div style={{marginTop:"10px",  fontWeight : "700", fontSize:"14px"}}>Cart</div>
                     </div>
                         
                 </div>
